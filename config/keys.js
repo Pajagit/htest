710eba65490e4176061aa765dfdfd42082469fbf
janaantic91 <jana.antic@htecgroup.com>
2019-09-08 16:22:38 +0200

    added config dev

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+
+    module.exports = require("./keys_dev");
+  
+  
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  postgresURI: 'postgres://postgres:postgres@localhost:5432/htest',
+  baseURL: 'http://localhost:3000/'
+};
