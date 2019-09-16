7a21e943afe4cb7659eb00f113cdf3331b86effc
Milos Najdanovic <milos.najdanovic@quinyx.com>
2019-09-16 13:01:14 +0200

    added if statement to search devices by office_id

--- routes/api/devices/getAllDevices.js
+++ routes/api/devices/getAllDevices.js
@@ -7,24 +7,27 @@ const Office = require("../../../models/office");
 // @desc all devices route
 // @access public
 module.exports = Router({ mergeParams: true }).get("/devices", (req, res) => {
-  if (isNaN(req.query.office_id)) {
-    res.status(400).json({ error: "Office id is not valid number" });
-  } else {
-    Device.findAll({
-      attributes: ["id", "title", "resolution", "dpi", "udid", "screen_size", "retina", "simulator", "deleted"],
-      where: {
-        deleted: false
-      },
-      include: [
-        {
-          model: Office,
-          attributes: ["id", "city"],
-          required: false
-        }
-      ],
-      order: [["title", "ASC"], [Office, "id", "ASC"]]
-    }).then(devices => {
-      res.json(devices);
-    });
+  var whereStatement = {};
+  if (req.query.office_id) {
+    if (isNaN(req.query.office_id)) {
+      res.status(400).json({ error: "Office id is not valid number" });
+    } else {
+      whereStatement.office_id = req.query.office_id;
+    }
   }
+  whereStatement.deleted = false;
+  Device.findAll({
+    attributes: ["id", "title", "resolution", "dpi", "udid", "screen_size", "retina", "simulator", "deleted"],
+    where: whereStatement,
+    include: [
+      {
+        model: Office,
+        attributes: ["id", "city"],
+        required: false
+      }
+    ],
+    order: [["title", "ASC"], [Office, "id", "ASC"]]
+  }).then(devices => {
+    res.json(devices);
+  });
 });
