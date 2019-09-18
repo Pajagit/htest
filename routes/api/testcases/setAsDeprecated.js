fc4e9eb3a594b0e1a03ca30f448493960bb399ef
janaantic91 <jana.antic@htecgroup.com>
2019-09-18 10:13:02 +0200

    Protect all routes

--- routes/api/testcases/setAsDeprecated.js
+++ routes/api/testcases/setAsDeprecated.js
@@ -1,34 +1,39 @@
 const Router = require("express").Router;
+const passport = require("passport");
 const isEmpty = require("../../../validation/is-empty");
 const TestCase = require("../../../models/testcase");
 
 // @route PUT api/testcases/:id/deprecated
 // @desc Update test case by id and set as deprecated
 // @access private
-module.exports = Router({ mergeParams: true }).put("/testcases/testcase/:id/deprecated", (req, res) => {
-  if (isNaN(req.params.id || isEmpty(req.params.id))) {
-    res.status(400).json({ error: "Test case id is not valid number" });
-  } else {
-    TestCase.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(testcase => {
-      if (!isEmpty(testcase)) {
-        testCaseFields = {};
-        testCaseFields.deprecated = true;
-        TestCase.update(testCaseFields, {
-          where: { id: testcase.id },
-          returning: true,
-          plain: true
-        })
-          .then(testcase => {
-            res.json(testcase[1]);
+module.exports = Router({ mergeParams: true }).put(
+  "/testcases/testcase/:id/deprecated",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (isNaN(req.params.id || isEmpty(req.params.id))) {
+      res.status(400).json({ error: "Test case id is not valid number" });
+    } else {
+      TestCase.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(testcase => {
+        if (!isEmpty(testcase)) {
+          testCaseFields = {};
+          testCaseFields.deprecated = true;
+          TestCase.update(testCaseFields, {
+            where: { id: testcase.id },
+            returning: true,
+            plain: true
           })
-          .catch(err => console.log(err));
-      } else {
-        res.status(404).json({ error: `Can not find test case with this ${req.params.id} id` });
-      }
-    });
+            .then(testcase => {
+              res.json(testcase[1]);
+            })
+            .catch(err => console.log(err));
+        } else {
+          res.status(404).json({ error: `Can not find test case with this ${req.params.id} id` });
+        }
+      });
+    }
   }
-});
+);
