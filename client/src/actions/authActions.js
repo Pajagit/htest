6c0b8bff0e2182678e5cbb71c690c3b782040b67
Pajagit <rustless12@gmail.com>
2019-09-18 14:17:19 +0200

    Added interceptor for refreshing token

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,7 +4,7 @@ import jwt_decode from "jwt-decode";
 // import { clearCurrentProfile } from "./profileActions";
 
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
-// import store from "../store";
+import store from "../store";
 
 // Login - Get User Token
 export const loginUser = userData => dispatch => {
@@ -51,3 +51,44 @@ export const logoutUser = () => dispatch => {
   // Set current user to {} which will also set isAuthenticated to false
   dispatch(setCurrentUser({}));
 };
+
+// Intercepting all requests and cheking if token is expired. If it is it will be refreshed
+axios.interceptors.request.use(async request => {
+  if (request.url !== "/api/users/login" && request.url !== "/api/users/token") {
+    var token = localStorage.getItem("jwtHtestToken");
+    const currentTime = Date.now() / 1000;
+    const decoded = jwt_decode(token);
+
+    var cryptedRefreshToken = localStorage.getItem("jwtHtestRefreshToken");
+
+    let refreshTokenObj = {
+      refreshToken: cryptedRefreshToken
+    };
+
+    if (decoded.exp - currentTime < 1) {
+      console.log("test");
+      await axios
+        .post("/api/token", refreshTokenObj)
+        .then(res => {
+          // Save to localStorage
+          const { token, refreshToken } = res.data;
+          // Set token to ls
+          localStorage.setItem("jwtHtestToken", token);
+          localStorage.setItem("jwtHtestRefreshToken", refreshToken);
+          // Set token to Auth header
+          setAuthToken(token);
+          // Decode token to get user data
+          const decoded = jwt_decode(token);
+          // Set current user
+          store.dispatch(setCurrentUser(decoded));
+          request.headers.Authorization = "Bearer " + token;
+        })
+        .catch(err => {
+          store.dispatch(logoutUser());
+          // Clear current profile
+          // store.dispatch(clearCurrentProfile());
+        });
+    }
+  }
+  return request;
+});
