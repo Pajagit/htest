3c9c2f472ef509240dfa130d604f2609c2252da7
janaantic91 <jana.antic@htecgroup.com>
2019-09-19 11:33:39 +0200

    Added image to payload on login and refresh token

--- routes/api/users/login.js
+++ routes/api/users/login.js
@@ -55,7 +55,8 @@ module.exports = Router({ mergeParams: true }).post("/users/login", (req, res) =
           id: user.id,
           first_name: newUserValues.first_name,
           last_name: newUserValues.last_name,
-          email: user.email
+          email: user.email,
+          image_url: newUserValues.image_url
         };
 
         function encrypt(text) {
