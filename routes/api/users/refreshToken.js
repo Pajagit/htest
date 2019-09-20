e8eed504f940771b5facc46ca642d27b1df37e85
janaantic91 <jana.antic@htecgroup.com>
2019-09-20 14:05:10 +0200

    User activation added and parameter active added to payload

--- routes/api/users/refreshToken.js
+++ routes/api/users/refreshToken.js
@@ -79,7 +79,8 @@ module.exports = Router({ mergeParams: true }).post("/token", (req, res) => {
           first_name: newUserValues.first_name ? newUserValues.first_name : user.first_name,
           last_name: newUserValues.last_name ? newUserValues.last_name : user.last_name,
           email: user.email,
-          image_url: newUserValues.image_url ? newUserValues.image_url : user.image_url
+          image_url: newUserValues.image_url ? newUserValues.image_url : user.image_url,
+          active: user.active
         };
 
         refreshToken = jwt.sign(payload, keys.secretOrKeyRefresh, {
