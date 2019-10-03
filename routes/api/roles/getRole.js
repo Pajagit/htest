d017447a415daf2ab6873078ded349d4426c8ad2
janaantic91 <jana.antic@htecgroup.com>
2019-10-03 17:46:15 +0200

    Started...

--- routes/api/roles/getRole.js
+++ routes/api/roles/getRole.js
@@ -1,6 +1,6 @@
 const Router = require("express").Router;
 const passport = require("passport");
-const Role = require("../../../models/role");
+const RoleController = require("../../../controllers/role");
 
 // @route GET api/roles/role/:id
 // @desc Get role by id
@@ -8,25 +8,5 @@ const Role = require("../../../models/role");
 module.exports = Router({ mergeParams: true }).get(
   "/roles/role/:id",
   passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    if (isNaN(req.params.id)) {
-      res.status(400).json({ error: "Role id is not valid number" });
-    } else {
-      Role.findOne({
-        attributes: ["id", "title"],
-        where: {
-          id: req.params.id
-        },
-        order: [["id", "DESC"]]
-      })
-        .then(role => {
-          if (role) {
-            res.status(200).json(role);
-          } else {
-            res.status(404).json({ message: "Role doesn't exist" });
-          }
-        })
-        .catch(err => console.log(err));
-    }
-  }
+  RoleController.getRoles
 );
