add76db60dfb3a02321a0a7f67bdfc6c568b8150
Pajagit <rustless12@gmail.com>
2019-10-25 21:55:53 +0200

    Added environment placeholder pages

--- client/src/pages/project/settings/environments/Environments.js
+++ client/src/pages/project/settings/environments/Environments.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
+import isEmpty from "../../../../validation/isEmpty";
 import { getGroups } from "../../../../actions/groupsActions";
+
+import PortraitEnvironment from "../../../../components/common/PortraitEnvironment";
 import GlobalPanel from "../../../../components/global-panel/GlobalPanel";
 import ProjectPanel from "../../../../components/project-panel/ProjectPanel";
 import BtnAnchor from "../../../../components/common/BtnAnchor";
 import Header from "../../../../components/common/Header";
+import Spinner from "../../../../components/common/Spinner";
 
 class Environments extends Component {
   constructor(props) {
@@ -22,6 +26,32 @@ class Environments extends Component {
     this.setState({ projectId: this.props.match.params.projectId });
   }
   render() {
+    // var { devices, loading } = this.props.devices;
+    var environments = [
+      { id: 1, title: "Test", url: "http://www.test-url.com" },
+      { id: 2, title: "Development", url: "http://www.development-url.com" },
+      { id: 3, title: "Staging", url: "http://www.staging-url.com" },
+      { id: 4, title: "Production", url: "http://www.production-url.com" },
+      { id: 5, title: "Fleet", url: "http://www.fleet-url.com" },
+      { id: 6, title: "SSO", url: "http://www.sso-url.com" }
+    ];
+    var content;
+
+    if (environments === null) {
+      content = <Spinner />;
+    } else if (!isEmpty(environments)) {
+      content = environments.map((browser, index) => (
+        <PortraitEnvironment
+          key={index}
+          title={browser.title}
+          url={browser.url}
+          id={browser.id}
+          projectId={this.props.match.params.projectId}
+        />
+      ));
+    } else if (isEmpty(environments)) {
+      content = <div className="testcase-container-no-content">There are no environments added yet</div>;
+    }
     return (
       <div className="wrapper">
         <GlobalPanel props={this.props} />
@@ -38,24 +68,11 @@ class Environments extends Component {
                 label="Add Environment"
                 disabled={true}
                 className={"a-btn a-btn-primary"}
-                link={`/${this.state.projectId}/CreateNewGroup`}
+                link={`/${this.state.projectId}/NewEnvironment`}
               />
             }
           />
-          <div className="list-item-container">
-            <div className="header">
-              <div className="header--title">Environments </div>
-              <div className="header--buttons">
-                <div className="header--buttons--primary"></div>
-                <div
-                  className="header--buttons--secondary clickable"
-                  //    onClick={e => this.confirmModal([])}
-                >
-                  {/* <i className="fas fa-trash-alt"></i> */}
-                </div>
-              </div>
-            </div>
-          </div>
+          <div className="testcase-grid testcase-container">{content}</div>
         </div>
       </div>
     );
