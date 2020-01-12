b186d7f6b4c269d5ff35e1bb6a9528df4629a229
Paja <rustless12@gmail.com>
2020-01-12 15:39:31 +0100

    Added logic for adding/removing simulators from project

--- client/src/pages/project/settings/simulators/Simulators.js
+++ client/src/pages/project/settings/simulators/Simulators.js
@@ -50,7 +50,7 @@ class Simulators extends Component {
   }
   changeIsUsed(id, used) {
     this.props.simulatorIsUsed(id, used, this.props.match.params.projectId, () => {
-      this.props.getSimulators();
+      this.props.getSimulators(this.props.match.params.projectId);
     });
   }
   render() {
