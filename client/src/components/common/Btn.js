1cec0bc33916599c3f78b47ddd96a43d4e6713e7
Paja <rustless12@gmail.com>
2020-03-07 22:36:31 +0100

    Added option to create report when creating TC, prevent clicking add to report if add new TC is checked and added option to choose where to go after report is created

--- client/src/components/common/Btn.js
+++ client/src/components/common/Btn.js
@@ -1,7 +1,11 @@
 import React from "react";
-function Btn({ type, className, label, onClick }) {
+function Btn({ type, className, label, onClick, disabled }) {
+  var classNameValue = className;
+  if (disabled) {
+    classNameValue = className + " disabled";
+  }
   return (
-    <button type={type} className={className} onClick={onClick}>
+    <button type={type} className={classNameValue} onClick={onClick} disabled={disabled}>
       {label}
     </button>
   );
