c634ed50df3d508f56d3a09db8796fdd236c18fd
Paja <rustless12@gmail.com>
2020-03-13 14:56:35 +0100

    Created hover popup for removed items in reports list for landscape and portrait components

--- client/src/components/common/PortraitReport.js
+++ client/src/components/common/PortraitReport.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReportStatus from "./ReportStatus";
+import ReactTooltip from "react-tooltip";
 import moment from "moment";
 
 function PortraitReport({
@@ -28,90 +29,119 @@ function PortraitReport({
   var commentValue = "";
   var actualResultValue = "";
   var simulatorValue = "";
-  if (device.title) {
+  if (device[0].title) {
     deviceValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Device:</div>
-        <div className="portrait-report-bottom-container--item-value">{device.title}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Device:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {device[0].title}{" "}
+          {device[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
-  if (browser.title) {
+  if (browser[0].title) {
     browserValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Browser:</div>
-        <div className="portrait-report-bottom-container--item-value">{browser.title}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Browser:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {browser[0].title}{" "}
+          {browser[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
-  if (version.version) {
+  if (version[0].version) {
     versionValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Version:</div>
-        <div className="portrait-report-bottom-container--item-value">{version.version}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Version:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {version[0].version}{" "}
+          {version[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
-  if (operatingsystem.title) {
+  if (operatingsystem[0].title) {
     operatingSystemValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Operating System:</div>
-        <div className="portrait-report-bottom-container--item-value">{operatingsystem.title}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Operating System:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {operatingsystem[0].title}{" "}
+          {operatingsystem[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
-  if (environment.title) {
+  if (environment[0].title) {
     environmentValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Environment:</div>
-        <div className="portrait-report-bottom-container--item-value">{environment.title}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Environment:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {environment[0].title}{" "}
+          {environment[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
   if (comment) {
     commentValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Comment:</div>
-        <div className="portrait-report-bottom-container--item-value">{comment}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Comment:</div>
+        <div className='portrait-report-bottom-container--item-value'>{comment}</div>
       </div>
     );
   }
   if (actual_result) {
     actualResultValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Actual Result:</div>
-        <div className="portrait-report-bottom-container--item-value">{actual_result}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Actual Result:</div>
+        <div className='portrait-report-bottom-container--item-value'>{actual_result}</div>
       </div>
     );
   }
-  if (simulator.title) {
+  if (simulator[0].title) {
     simulatorValue = (
-      <div className="portrait-report-bottom-container--item">
-        <div className="portrait-report-bottom-container--item-title">Simulator:</div>
-        <div className="portrait-report-bottom-container--item-value">{simulator.title}</div>
+      <div className='portrait-report-bottom-container--item'>
+        <div className='portrait-report-bottom-container--item-title'>Simulator:</div>
+        <div className='portrait-report-bottom-container--item-value'>
+          {simulator[0].title}{" "}
+          {simulator[1] ? <i className='fas fa-info-circle removed-text' data-tip data-for='removed'></i> : ""}
+        </div>
       </div>
     );
   }
   return (
-    <div className="portrait-report clickable" onClick={onClick}>
-      <div className="portrait-report-top">
-        <div className="portrait-report-top-container">
-          <div className="portrait-report-top-container--title">{title}</div>
+    <div className='portrait-report clickable' onClick={onClick}>
+      <div className='portrait-report-top'>
+        <div className='portrait-report-top-container'>
+          <div className='portrait-report-top-container--title'>{title}</div>
 
-          <div className="portrait-report-top-container--tags">{tags}</div>
+          <div className='portrait-report-top-container--tags'>{tags}</div>
 
-          <div className="portrait-report-top-container--author">
+          <div className='portrait-report-top-container--author'>
             {author.first_name} {author.last_name} {author.position ? ", " + author.position : ""}
           </div>
-          <div className="portrait-report-top-container--date">{moment(date).format("Do MMMM YYYY, h:mm:ss a")}</div>
-          <div className="portrait-report-top-container--button">
+          <div className='portrait-report-top-container--date'>{moment(date).format("Do MMMM YYYY, h:mm:ss a")}</div>
+          <div className='portrait-report-top-container--button'>
             <ReportStatus status={status.title.toLowerCase()} id={id} />
           </div>
         </div>
       </div>
-
-      <div className="portrait-report-bottom">
-        <div className="portrait-report-bottom-container">
+      <ReactTooltip
+        id='removed'
+        aria-haspopup='true'
+        className='custom-color-no-arrow'
+        textColor='#fff'
+        backgroundColor='#4d3cb5'
+        effect='solid'
+      >
+        <p>This setup item is removed.</p>
+        <p>You can not filter or create</p>
+        <p> new report with this item.</p>
+      </ReactTooltip>
+      <div className='portrait-report-bottom'>
+        <div className='portrait-report-bottom-container'>
           {deviceValue}
           {browserValue}
           {versionValue}
