bc017248c30fc55f2a9f8f540208a52e7b0bbfbc
Paja <rustless12@gmail.com>
2020-03-16 07:04:36 +0100

    Additional checks for statistics

--- client/src/actions/statisticActions.js
+++ client/src/actions/statisticActions.js
@@ -6,8 +6,12 @@ import { GET_PROJECT_STATISTICS, STATISTICS_LOADING } from "./types";
 export const getProjectStatistics = () => dispatch => {
   dispatch(statisticsLoading());
   axios
-    // .get(`https://api.myjson.com/bins/1citmm`)
-    .get(`https://api.myjson.com/bins/1afdku`)
+    // .get("https://api.myjson.com/bins/uhvtm") // annual test
+    .get("https://api.myjson.com/bins/18wknu") // most used TC test
+    // .get("https://api.myjson.com/bins/w81h6") // nema ni jedan
+    // .get("https://api.myjson.com/bins/m1j16") // ima po neki
+    // .get(`https://api.myjson.com/bins/1citmm`) // ima sve
+    // .get(`https://api.myjson.com/bins/1afdku`)
 
     .then(res =>
       dispatch({
