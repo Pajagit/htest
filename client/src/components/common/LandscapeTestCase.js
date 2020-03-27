227cebb435a2a47faa2b98d5662e30a52eba043a
Paja <rustless12@gmail.com>
2020-03-27 18:10:40 +0100

    Limited title and groups on testcases for both landscape and portrait

--- client/src/components/common/LandscapeTestCase.js
+++ client/src/components/common/LandscapeTestCase.js
@@ -1,33 +1,46 @@
 import React from "react";
 import AddReportBtn from "./AddReportBtn";
+import Tag from "./Tag";
 import moment from "moment";
 
 function LandscapeTestCase({ title, tags, author, date, description, id, projectId, onClick }) {
+  var tagsToShow = 8;
+  var spliceCount = 0;
+  var totalCount = tags.length;
+  var array = tags;
+  if (tags.length > tagsToShow) {
+    array.splice(tagsToShow, totalCount);
+    spliceCount = totalCount - tags.length;
+  }
+  if (spliceCount > 0) {
+    tags.push(<Tag title={`+ ${spliceCount} other`} color={"PRIMARY"} />);
+  }
+
   return (
-    <div className="landscape-testcase clickable" onClick={onClick}>
-      <div className="landscape-testcase-left">
-        <div className="landscape-testcase-left-container">
-          <div className="landscape-testcase-left-container--title">{title}</div>
+    <div className='landscape-testcase clickable' onClick={onClick}>
+      <div className='landscape-testcase-left'>
+        <div className='landscape-testcase-left-container'>
+          <div className='landscape-testcase-left-container--title'>{title}</div>
 
-          <div className="landscape-testcase-left-container--tags">{tags}</div>
+          <div className='landscape-testcase-left-container--tags'>{tags}</div>
 
-          <div className="landscape-testcase-left-container--author">
+          <div className='landscape-testcase-left-container--author'>
             {author.first_name} {author.last_name} {author.position ? ", " + author.position : ""}
           </div>
-          <div className="landscape-testcase-left-container--date">
+          <div className='landscape-testcase-left-container--date'>
             {moment(date).format("Do MMMM YYYY, h:mm:ss a")}
           </div>
-          <div className="landscape-testcase-left-container--button">
+          <div className='landscape-testcase-left-container--button'>
             <AddReportBtn title={"Add to Report"} id={id} />
           </div>
         </div>
       </div>
 
-      <div className="landscape-testcase-right">
-        <div className="landscape-testcase-right-container">
-          <div className="landscape-testcase-right-container--description">
-            <div className="landscape-testcase-right-container--description-title">Description:</div>
-            <div className="landscape-testcase-right-container--description-value">{description}</div>
+      <div className='landscape-testcase-right'>
+        <div className='landscape-testcase-right-container'>
+          <div className='landscape-testcase-right-container--description'>
+            <div className='landscape-testcase-right-container--description-title'>Description:</div>
+            <div className='landscape-testcase-right-container--description-value'>{description}</div>
           </div>
         </div>
       </div>
--- client/src/components/common/PortraitTestCase.js
+++ client/src/components/common/PortraitTestCase.js
@@ -1,34 +1,47 @@
 import React from "react";
 import AddReportBtn from "../common/AddReportBtn";
+import Tag from "./Tag";
 import moment from "moment";
 
 function PortraitTestCase({ title, tags, author, date, description, id, onClick, isValidWrite, onClickAddReport }) {
   var addReportBtn = "";
   if (isValidWrite) {
     addReportBtn = (
-      <div className="portrait-testcase-bottom-container--button" onClick={onClickAddReport}  >
+      <div className='portrait-testcase-bottom-container--button' onClick={onClickAddReport}>
         <AddReportBtn title={"Add to Report"} id={id} />
       </div>
     );
   }
+
+  var tagsToShow = 8;
+  var spliceCount = 0;
+  var totalCount = tags.length;
+  var array = tags;
+  if (tags.length > tagsToShow) {
+    array.splice(tagsToShow, totalCount);
+    spliceCount = totalCount - tags.length;
+  }
+  if (spliceCount > 0) {
+    tags.push(<Tag title={`+ ${spliceCount} other`} color={"PRIMARY"} />);
+  }
   return (
-    <div className="portrait-testcase clickable" onClick={onClick} >
-      <div className="portrait-testcase-top">
-        <div className="portrait-testcase-top-container">
-          <div className="portrait-testcase-top-container--title">{title}</div>
+    <div className='portrait-testcase clickable' onClick={onClick}>
+      <div className='portrait-testcase-top'>
+        <div className='portrait-testcase-top-container'>
+          <div className='portrait-testcase-top-container--title'>{title}</div>
 
-          <div className="portrait-testcase-top-container--tags">{tags}</div>
+          <div className='portrait-testcase-top-container--tags'>{tags}</div>
 
-          <div className="portrait-testcase-top-container--author">
+          <div className='portrait-testcase-top-container--author'>
             {author.first_name} {author.last_name} {author.position ? ", " + author.position : ""}
           </div>
-          <div className="portrait-testcase-top-container--date">{moment(date).format("Do MMMM YYYY, h:mm:ss a")}</div>
+          <div className='portrait-testcase-top-container--date'>{moment(date).format("Do MMMM YYYY, h:mm:ss a")}</div>
         </div>
       </div>
 
-      <div className="portrait-testcase-bottom">
-        <div className="portrait-testcase-bottom-container">
-          <div className="portrait-testcase-bottom-container--description">{description}</div>
+      <div className='portrait-testcase-bottom'>
+        <div className='portrait-testcase-bottom-container'>
+          <div className='portrait-testcase-bottom-container--description'>{description}</div>
           {addReportBtn}
         </div>
       </div>
