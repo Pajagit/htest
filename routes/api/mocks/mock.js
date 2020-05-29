2944445e0b6123574dbaaed9949c27921858f7be
Paja <rustless12@gmail.com>
2020-05-29 13:05:59 +0200

    Mocked ep

--- routes/api/mocks/mock.js
+++ routes/api/mocks/mock.js
@@ -7,242 +7,41 @@ const Office = require("../../../models/office");
 // @access public
 module.exports = Router({ mergeParams: true }).get(
   "/global-statistics-mocked",
-  passport.authenticate("jwt", { session: false }),
+
   (req, res) => {
     var globalSettings = JSON.parse(`{
-        "most_active_projects": [
-          {
-            "percentage": 33,
-            "title": "Stena"
+      "QueryResponse": {
+        "maxResults": "1",
+        "startPosition": "1",
+        "Employee": {
+          "Organization": "false",
+          "Title": "Mrs.",
+          "GivenName": "Jane",
+          "MiddleName": "Lane",
+          "FamilyName": "Doe",
+          "DisplayName": "Jane Lane Doe",
+          "PrintOnCheckName": "Jane Lane Doe",
+          "Active": "true",
+          "PrimaryPhone": { "FreeFormNumber": "505.555.9999" },
+          "PrimaryEmailAddr": { "Address": "janedoe@example.com" },
+          "EmployeeType": "Regular",
+          "status": "Synchronized",
+          "Id": "ABC123",
+          "SyncToken": "1",
+          "MetaData": {
+            "CreateTime": "2015-04-26T19:45:03Z",
+            "LastUpdatedTime": "2015-04-27T21:48:23Z"
           },
-          {
-            "percentage": 23,
-            "title": "Quinyx"
-          },
-          {
-            "percentage": 20,
-            "title": "Jammcard"
-          },
-          {
-            "percentage": 14,
-            "title": "AloeCare"
-          },
-          {
-            "percentage": 10,
-            "title": "Hrm"
+          "PrimaryAddr": {
+            "Line1": "123 Any Street",
+            "City": "Any City",
+            "CountrySubDivisionCode": "WA",
+            "PostalCode": "01234"
           }
-        ],
-        "most_user_reports": [
-          {
-            "failed": 68,
-            "total": 123,
-            "passed": 55,
-            "title": "Jana Antic"
-          },
-          {
-            "failed": 15,
-            "total": 98,
-            "passed": 83,
-            "title": "Sandra Jeremenkovic"
-          },
-          {
-            "failed": 67,
-            "total": 87,
-            "passed": 20,
-            "title": "Uros Jeremic"
-          },
-          {
-            "failed": 33,
-            "total": 66,
-            "passed": 33,
-            "title": "Milos Decov"
-          },
-          {
-            "failed": 5,
-            "total": 52,
-            "passed": 47,
-            "title": "Aleksandar Pavlovic"
-          }
-        ],
-        "annual_report": [
-            {
-                "total": 673,
-                "passed": 321,
-                "failed": 352,
-                "month": "April"
-              },
-              {
-                "total": 452,
-                "passed": 221,
-                "failed": 231,
-                "month": "May"
-              },
-              {
-                "total": 678,
-                "passed": 322,
-                "failed": 356,
-                "month": "June"
-              },
-              {
-                "total": 657,
-                "passed": 456,
-                "failed": 201,
-                "month": "July"
-              },
-              {
-                "total": 544,
-                "passed": 421,
-                "failed": 123,
-                "month": "August"
-              },
-              {
-                "total": 511,
-                "passed": 210,
-                "failed": 301,
-                "month": "September"
-              },
-              {
-                "total": 498,
-                "passed": 267,
-                "failed": 231,
-                "month": "October"
-              },
-              {
-                "total": 590,
-                "passed": 345,
-                "failed": 245,
-                "month": "November"
-              },
-              {
-                "total": 378,
-                "passed": 115,
-                "failed": 263,
-                "month": "December"
-              },
-              {
-                "total": 669,
-                "passed": 331,
-                "failed": 338,
-                "month": "January"
-              },
-              {
-                "total": 521,
-                "passed": 251,
-                "failed": 270,
-                "month": "February"
-              },
-              {
-                "total": 27,
-                "passed": 15,
-                "failed": 12,
-                "month": "March"
-              }
-            ],
-        "most_testcases_failed": [
-          {
-            "failed": 168,
-            "total": 223,
-            "passed": 55,
-            "title": "Jammcard"
-          },
-          {
-            "failed": 78,
-            "total": 143,
-            "passed": 65,
-            "title": "Quinyx"
-          },
-          {
-            "failed": 68,
-            "total": 123,
-            "passed": 55,
-            "title": "HRM"
-          },
-          {
-            "failed": 60,
-            "total": 115,
-            "passed": 47,
-            "title": "AloeCare"
-          },
-          {
-            "failed": 48,
-            "total": 103,
-            "passed": 35,
-            "title": "Stena"
-          }
-        ],
-        "total_data": {
-          "total_passed_reports": {
-            "percentage": -11.1,
-            "value": 16533
-          },
-          "total_testcases": {
-            "percentage": -22.4,
-            "value": 12412
-          },
-          "total_failed_reports": {
-            "percentage": 33.3,
-            "value": 6807
-          },
-          "total_reports": {
-            "percentage": -44.1,
-            "value": 22341
-          }
-        },
-        "most_version_failed": [
-          {
-            "failed": 3,
-            "total": 7,
-            "passed": 4,
-            "title": 2.1
-          },
-          {
-            "failed": 3,
-            "total": 7,
-            "passed": 4,
-            "title": 3.4
-          },
-          {
-            "failed": 3,
-            "total": 7,
-            "passed": 4,
-            "title": 1.2
-          },
-          {
-            "failed": 3,
-            "total": 7,
-            "passed": 4,
-            "title": 4.1
-          },
-          {
-            "failed": 3,
-            "total": 7,
-            "passed": 4,
-            "title": 2.6
-          }
-        ],
-        "most_user_testcases": [
-          {
-            "total": 5,
-            "title": "Jana Antic"
-          },
-          {
-            "total": 4,
-            "title": "Sandra Jeremenkovic"
-          },
-          {
-            "total": 3,
-            "title": "Uros Jeremic"
-          },
-          {
-            "total": 2,
-            "title": "Milos Decov"
-          },
-          {
-            "total": 1,
-            "title": "Aleksandar Pavlovic"
-          }
-        ]
-      }`);
+        }
+      },
+      "time": "2015-04-27T22:12:32.012Z"
+    }`);
     res.json(globalSettings);
   }
 );
